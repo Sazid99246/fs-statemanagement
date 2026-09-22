674b9c85aeddcdefbcd6eb092cad33790afae5c5
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-22 20:47:15 +0600

    Exercise 1 and 2 completed

--- unicafe/src/components/Buttons.jsx
+++ unicafe/src/components/Buttons.jsx
@@ -1,10 +1,16 @@
+import useStore from './store'
+
 const Buttons = () => {
+  const increaseGood = useStore(state => state.increaseGood)
+  const increaseNeutral = useStore(state => state.increaseNeutral)
+  const increaseBad = useStore(state => state.increaseBad)
+
   return (
     <div>
       <h2>give feedback</h2>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={increaseGood}>good</button>
+      <button onClick={increaseNeutral}>neutral</button>
+      <button onClick={increaseBad}>bad</button>
     </div>
   )
 }
